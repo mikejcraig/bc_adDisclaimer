96c35403d67151918c580c501ad2d013d4028972
mikejcraig <mike.craig@icloud.com>
2019-04-27 23:16:58 -0400

    Update bc-plugin-externalSrc.js

--- bc-plugin-externalSrc.js
+++ bc-plugin-externalSrc.js
@@ -1,7 +1,8 @@
 videojs.registerPlugin('externalSrc', function() {
   var player = this;
-  var playerSource = (player.tagAttributes.src) ? player.tagAttributes.src : false;
-  if(playerSource){
-    player.src(playerSource)
-  }
+//  var playerSource = (player.tagAttributes.src) ? player.tagAttributes.src : false;
+//  if(playerSource){
+ //   player.src(playerSource)
+//  }
+  player.src("https://arenax-blobstorage.cdn.arkadiumhosted.com/arenax-index/_arena-shared-content_/tiny3.mp4")
 });

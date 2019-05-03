eb21fc1964390a1c3c861018d2b72e6a60dadee0
mikejcraig <mike.craig@icloud.com>
2019-05-03 14:52:14 -0400

    Add files via upload

---
+++ bc-plugin-adtext-prod.js
@@ -0,0 +1,35 @@
+videojs.registerPlugin('adDisclaimer', function() {
+  var player = this;
+  player.on('loadedmetadata', function() {
+    player.on('ima3-started', function() {
+    alert('start');
+      function adDisclaimerElement() {
+        var disclaimerEl = document.createElement('div');
+        disclaimerEl.setAttribute("id", "ad-disclaimer");
+        disclaimerEl.innerHTML = "Advertisement";
+            alert(disclaimerEl);
+
+        return disclaimerEl;
+  }
+      function spacerElement() {
+        var spacerEl = document.getElementsByClassName('vjs-spacer')[1];
+        return spacerEl;
+      }
+      function appendAdDisclaimer(spacerEl, disclaimerEl) {
+        spacerEl.appendChild(disclaimerEl);
+      }
+      function showAdDisclaimer() {
+        appendAdDisclaimer(spacerElement(), adDisclaimerElement());
+      }
+      showAdDisclaimer();
+    });
+    player.on('ima3-complete', function() {
+              alert('complete');
+      function hideAdDisclaimer() {
+        var disclaimerEl = document.getElementById('ad-disclaimer');
+        disclaimerEl.remove();
+      }
+      hideAdDisclaimer();
+    });
+}); 
+});

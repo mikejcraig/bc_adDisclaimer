af63be39f8249ad1b9cbce2fbc0cbd54ba2f405f
mikejcraig <mike.craig@icloud.com>
2019-05-03 15:08:04 -0400

    Update bc-plugin-adtext-prod.js

--- bc-plugin-adtext-prod.js
+++ bc-plugin-adtext-prod.js
@@ -2,13 +2,10 @@ videojs.registerPlugin('adDisclaimer', function() {
   var player = this;
   player.on('loadedmetadata', function() {
     player.on('ima3-started', function() {
-    alert('start');
       function adDisclaimerElement() {
         var disclaimerEl = document.createElement('div');
         disclaimerEl.setAttribute("id", "ad-disclaimer");
         disclaimerEl.innerHTML = "Advertisement";
-            alert(disclaimerEl);
-
         return disclaimerEl;
   }
       function spacerElement() {
@@ -24,7 +21,6 @@ videojs.registerPlugin('adDisclaimer', function() {
       showAdDisclaimer();
     });
     player.on('ima3-complete', function() {
-              alert('complete');
       function hideAdDisclaimer() {
         var disclaimerEl = document.getElementById('ad-disclaimer');
         disclaimerEl.remove();

901914b5f0a61a2074aab663f33b9d07291515f8
mikejcraig <mike.craig@icloud.com>
2019-05-05 15:45:25 -0400

    Add files via upload

---
+++ brightcove-plugin-disclaimer-text.babel.js
@@ -0,0 +1,31 @@
+const player = this;
+
+player.on('loadedmetadata', () => {
+  player.on('ima3-started', () => {
+    alert('');
+    const adDisclaimerElement = () => {
+      const disclaimerEl = document.createElement('h3');
+      disclaimerEl.setAttribute("class", "ad-disclaimer");
+      disclaimerEl.innerText = "Advertisement";
+      return disclaimerEl;
+    }
+    const spacerElement = () => {
+      const spacerEl = document.getElementsByClassName('vjs-spacer')[1];
+      return spacerEl;
+    }
+    const appendAdDisclaimer = (spacerEl, disclaimerEl) => {
+      spacerEl.appendChild(disclaimerEl);
+    }
+    const showAdDisclaimer = () => {
+      appendAdDisclaimer(spacerElement(), adDisclaimerElement());
+    }
+    showAdDisclaimer();
+  });
+  player.on('ima3-complete', () => {
+    const hideAdDisclaimer = () => {
+      const disclaimerEl = document.getElementsByClassName('ad-disclaimer');
+      disclaimerEl.remove();
+    }
+    hideAdDisclaimer();
+  });
+});

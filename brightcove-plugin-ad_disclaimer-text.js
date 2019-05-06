362fba2d054b8eabf1694c8786e040e55ac0b59a
mikejcraig <mike.craig@icloud.com>
2019-05-06 17:51:52 -0400

    Add files via upload

---
+++ brightcove-plugin-ad_disclaimer-text.js
@@ -0,0 +1,38 @@
+"use strict";
+
+videojs.registerPlugin('adDisclaimer', function () {
+  var _this = this;
+
+  this.on('loadedmetadata', function () {
+    var playerId = _this.id();
+
+    var playerControlBarContainer = document.querySelectorAll('#' + playerId + ' .vjs-spacer')[1];
+
+    _this.on('ima3-started', function () {
+      var adDisclaimerElement = function adDisclaimerElement() {
+        var disclaimerEl = document.createElement('p');
+        disclaimerEl.setAttribute('class', 'ad-disclaimer');
+        disclaimerEl.innerText = 'Advertisement';
+        return disclaimerEl;
+      };
+
+      var appendAdDisclaimer = function appendAdDisclaimer(spacerEl, disclaimerEl) {
+        spacerEl.appendChild(disclaimerEl);
+      };
+
+      var showAdDisclaimer = function showAdDisclaimer() {
+        appendAdDisclaimer(playerControlBarContainer, adDisclaimerElement());
+      };
+
+      showAdDisclaimer();
+    });
+
+    _this.on('ima3-complete', function () {
+      var hideAdDisclaimer = function hideAdDisclaimer() {
+        playerControlBarContainer.remove();
+      };
+
+      hideAdDisclaimer();
+    });
+  });
+});

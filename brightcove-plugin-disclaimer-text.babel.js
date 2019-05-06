7bfda6814225ef9566523a08b9f304975dff25f6
mikejcraig <mike.craig@icloud.com>
2019-05-06 14:31:48 -0400

    Update brightcove-plugin-disclaimer-text.babel.js

--- brightcove-plugin-disclaimer-text.babel.js
+++ brightcove-plugin-disclaimer-text.babel.js
@@ -1,13 +1,11 @@
-
 videojs.registerPlugin('adDisclaimer', function() {
-const player = this;
 
 this.on('loadedmetadata', () => {
   this.on('ima3-started', () => {
     alert(this.id());
     const playerId = this.id();
-    const playerControlBarContainer = document.querySelectorAll('#' + this.id() + ' .vjs-custom-control-spacer.vjs-spacer')[0];
-    
+    const playerControlBarContainer = document.querySelectorAll('#' + this.id() + ' .vjs-spacer')[1];
+
     const adDisclaimerElement = () => {
       const disclaimerEl = document.createElement('p');
       disclaimerEl.setAttribute('class', 'ad-disclaimer');
@@ -15,7 +13,7 @@ this.on('loadedmetadata', () => {
       return disclaimerEl;
     };
     const spacerElement = () => {
-      const spacerEl = playerControlBarContainer;
+      const spacerEl = document.getElementsByClassName('vjs-spacer')[1];
       // const spacerElParent = this.player().controlBar.customControlSpacer.id();
       // const spacerEl = document.getElementById(spacerElParent);
       return spacerEl;

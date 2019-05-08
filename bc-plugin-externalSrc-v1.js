15d426b7908474b4290f06fcda0de58bcc7bb66a
mikejcraig <mike.craig@icloud.com>
2019-05-08 16:21:40 -0400

    Add files via upload

--- bc-plugin-externalSrc-v1.js
+++ bc-plugin-externalSrc-v1.js
@@ -1,6 +1,6 @@
 videojs.registerPlugin('externalSrc', function() {
   var player = this;
-  var playerSource = (player.tagAttributes.src) ? player.tagAttributes.src : "http://arenax-blobstorage.cdn.arkadiumhosted.com/arenax-index/_arena-shared-content_/tiny3.mp4";
+  var playerSource = (player.tagAttributes.src) ? player.tagAttributes.src : false;
   if(playerSource){
     console.log('playerSource' + playerSource);
     player.src(playerSource);
@@ -9,16 +9,16 @@ videojs.registerPlugin('externalSrc', function() {
     var myPlayer = this;
     var playerId = myPlayer.id();
     var playerContainerId = document.getElementById(player.children()[0].id);
-    playerContainerId.style.display = "none";
+    // playerContainerId.style.display = "none";
 
   });
         player.on('ima3-loaded', function() {
       var myplayer = this;
       var playerId = myplayer.id();
-          console.log(playerId);
-                    console.log(document.getElementById(playerId));
+      //     console.log(playerId);
+      //               console.log(document.getElementById(playerId));
 
-      document.getElementById(playerId).style.minHeight = "360px";
+      // document.getElementById(playerId).style.minHeight = "360px";
       });
 
 });

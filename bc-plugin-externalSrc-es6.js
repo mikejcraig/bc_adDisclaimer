5930507923cc4d8f5771966f0aa95fa9d5bc531d
mikejcraig <mike.craig@icloud.com>
2019-05-08 11:49:04 -0400

    Add files via upload

--- bc-plugin-externalSrc-es6.js
+++ bc-plugin-externalSrc-es6.js
@@ -1,6 +1,6 @@
 videojs.registerPlugin('externalSrc', function() {
   var player = this;
-  var playerSource = (player.tagAttributes.src) ? player.tagAttributes.src : false;
+  var playerSource = (player.tagAttributes.src) ? player.tagAttributes.src : "https://arenax-blobstorage.cdn.arkadiumhosted.com/arenax-index/_arena-shared-content_/tiny3.mp4";
   if(playerSource){
     player.src(playerSource)
   }

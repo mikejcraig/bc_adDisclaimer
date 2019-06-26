95260df1f77b4009d754bff5c897498eb0ce5a78
mikejcraig <mike.craig@icloud.com>
2019-06-26 12:19:11 -0400

    Add files via upload

---
+++ tablet_fix/tablet-touch-plugin.js
@@ -0,0 +1,24 @@
+"use strict";
+
+videojs.registerPlugin('adDisclaimer', function () {
+  var _this = this;
+
+  this.on('loadedmetadata', function () {
+    var playerId = _this.id();
+
+    _this.on('ima3-started', function () {
+        var windowWidthScrn = document.documentElement.clientWidth;
+
+        if (windowWidthScrn > 767 && windowWidthScrn < 1025) {
+        console.log('tablet');
+        $aarpe('.vjs-control-bar, .vjs-big-play-button').css('display','none');
+        $aarpe('.bc-style-JlvJnROgWL-default').remove();
+        }
+
+    });
+
+    _this.on('ima3-complete', function () {
+      $aarpe('.vjs-big-play-button').css('display','none');
+    });
+  });
+});

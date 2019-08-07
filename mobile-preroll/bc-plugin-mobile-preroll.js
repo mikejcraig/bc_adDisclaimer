b4847f3f99980ec62669288a5d90d6b160632f5f
mikejcraig <mike.craig@icloud.com>
2019-08-07 12:55:26 -0400

    Update bc-plugin-mobile-preroll.js

--- mobile-preroll/bc-plugin-mobile-preroll.js
+++ mobile-preroll/bc-plugin-mobile-preroll.js
@@ -2,15 +2,17 @@ videojs.registerPlugin('mobilePreroll', function() {
   var player = this;
 
   player.on('loadedmetadata', function() {
-    player.ima3({
-  ima3SdkSettings: {
-    "disableCustomPlaybackForIOS10Plus": true
-  }
-});
+
    window.google.ima.settings.setDisableCustomPlaybackForIOS10Plus(true);
    // window.google.ima.settings.l = true
 
 
 
 }); 
+      player.on('ima3-ready', function () {
+    player.ima3({
+  ima3SdkSettings: {
+    "disableCustomPlaybackForIOS10Plus": true
+  }
+});
 });

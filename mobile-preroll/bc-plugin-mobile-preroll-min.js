6bbcdbf2012980a6ab42c21ca3a2908f6aa35465
mikejcraig <mike.craig@icloud.com>
2019-08-18 17:55:41 -0400

    Update bc-plugin-mobile-preroll-min.js

--- mobile-preroll/bc-plugin-mobile-preroll-min.js
+++ mobile-preroll/bc-plugin-mobile-preroll-min.js
@@ -16,14 +16,16 @@
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 videojs.registerPlugin('mobileAdsPreroll', function () {
+  // Find the player
   var player = document.getElementById(this.id());
-  var playerContainer = player.firstElementChild;
+  var playerContainer = player.firstElementChild; // Add the playsinline attribute to video container inside the player
 
-  if (playerContainer) {
+  if (!!playerContainer && playerContainer.nodeName === 'VIDEO') {
     playerContainer.setAttribute('playsinline', '');
   }
 
   this.on('loadedmetadata', function () {
+    // Disable custom playback for ios
     if (_typeof(window.google.ima) === 'object') {
       window.google.ima.settings.setDisableCustomPlaybackForIOS10Plus(true);
     }

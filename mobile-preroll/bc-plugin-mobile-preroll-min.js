ea8dc40cead796919667326f515c6bf41a7e0b37
mikejcraig <mike.craig@icloud.com>
2019-08-18 16:24:12 -0400

    Update bc-plugin-mobile-preroll-min.js

--- mobile-preroll/bc-plugin-mobile-preroll-min.js
+++ mobile-preroll/bc-plugin-mobile-preroll-min.js
@@ -1,12 +1,31 @@
-videojs.registerPlugin('mobilePreroll', function() {
-  var player = this;
-x = this.id_;
-y = document.getElementById(x);
-y.firstElementChild.setAttribute("playsinline", "");
+// videojs.registerPlugin('mobilePreroll', function() {
+//   var player = this;
+// x = this.id_;
+// y = document.getElementById(x);
+// y.firstElementChild.setAttribute("playsinline", "");
   
-  player.on('loadedmetadata', function() {
-if(typeof window.google.ima == "object"){
-   window.google.ima.settings.setDisableCustomPlaybackForIOS10Plus(true);
-}
+//   player.on('loadedmetadata', function() {
+// if(typeof window.google.ima == "object"){
+//    window.google.ima.settings.setDisableCustomPlaybackForIOS10Plus(true);
+// }
+// });
+//       });
+
+
+"use strict";
+
+var _this = void 0;
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+videojs.registerPlugin('mobileAdsPreroll', function () {
+  var x = _this.id_;
+  var y = document.getElementById(x);
+  y.firstElementChild.setAttribute('playsinline', '');
+
+  _this.on('loadedmetadata', function () {
+    if (_typeof(window.google.ima) == 'object') {
+      window.google.ima.settings.setDisableCustomPlaybackForIOS10Plus(true);
+    }
+  });
 });
-      });

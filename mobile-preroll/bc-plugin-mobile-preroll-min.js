eb1422cde48294a7a4bc755baf2b51660373deca
mikejcraig <mike.craig@icloud.com>
2019-08-18 14:53:32 -0400

    Add files via upload

---
+++ mobile-preroll/bc-plugin-mobile-preroll-min.js
@@ -0,0 +1,12 @@
+videojs.registerPlugin('mobileAdsPreroll', function() {
+  var player = this;
+x = this.id_;
+y = document.getElementById(x);
+y.firstElementChild.setAttribute("playsinline", "");
+  player.on('loadedmetadata', function() {
+
+   window.google.ima.settings.setDisableCustomPlaybackForIOS10Plus(true);
+   // window.google.ima.settings.l = true
+});
+        window.google.ima.settings.l = true;
+      });

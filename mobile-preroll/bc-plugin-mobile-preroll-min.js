3c8e3fb7766351b28aa08cb9d9d9f8f166f2818c
mikejcraig <mike.craig@icloud.com>
2019-08-18 16:48:05 -0400

    Update bc-plugin-mobile-preroll-min.js

--- mobile-preroll/bc-plugin-mobile-preroll-min.js
+++ mobile-preroll/bc-plugin-mobile-preroll-min.js
@@ -11,19 +11,15 @@
 // });
 //       });
 
-
 "use strict";
-
-var _this = void 0;
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-videojs.registerPlugin('mobilePreroll', function () {
-  var x = _this.id_;
+videojs.registerPlugin('mobileAdsPreroll', function () {
+  var x = this.id_;
   var y = document.getElementById(x);
   y.firstElementChild.setAttribute('playsinline', '');
-
-  _this.on('loadedmetadata', function () {
+  this.on('loadedmetadata', function () {
     if (_typeof(window.google.ima) == 'object') {
       window.google.ima.settings.setDisableCustomPlaybackForIOS10Plus(true);
     }

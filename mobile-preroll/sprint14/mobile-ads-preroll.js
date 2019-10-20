8eb9b55727b7a1d38b67ef25fe1cfb994d80e672
mikejcraig <mike.craig@icloud.com>
2019-10-20 15:00:57 -0400

    Add files via upload

---
+++ mobile-preroll/sprint14/mobile-ads-preroll.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+videojs.registerPlugin('mobileAdsPreroll', function () {
+  // Find the player
+  var player = document.getElementById(this.id());
+  var playerContainer = player.firstElementChild;
+  var isMobileViewport = document.documentElement.clientWidth < 768;
+
+  if (isMobileViewport) {
+    // Add the playsinline attribute to video container inside the player
+    if (!!playerContainer && playerContainer.nodeName === 'VIDEO') {
+      playerContainer.setAttribute('playsinline', '');
+      console.log('1. playsinline');
+    }
+
+    this.on('loadedmetadata', function () {
+      // Disable custom playback for ios
+      if (_typeof(window.google.ima) === 'object') {
+        window.google.ima.settings.setDisableCustomPlaybackForIOS10Plus(true);
+      }
+    });
+    this.on('ima3-ready', function () {
+      // var windowWidthScrn = document.documentElement.clientWidth;
+      // if (windowWidthScrn < 768) {
+      if (_typeof(window.google.ima) === 'object') {
+        window.google.ima.settings.setDisableCustomPlaybackForIOS10Plus(true);
+        console.log('2. setDisableCustomPlaybackForIOS10Plus');
+      } // }
+
+    });
+    this.on('ads-ad-skipped', function () {
+      player.classList.add('not-hover');
+    });
+    this.on('ads-ad-ended', function () {
+      console.log('3. ads-ad-skipped');
+      console.log('4. ' + player.classList);
+      player.classList.add('not-hover');
+    });
+  }
+});

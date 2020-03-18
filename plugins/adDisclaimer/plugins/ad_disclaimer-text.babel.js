f88d2ae4848f8af648cc38c2dbbc24d31ab6443a
mikejcraig <mike.craig@icloud.com>
2020-03-18 15:14:08 -0400

    Add files via upload

---
+++ plugins/adDisclaimer/plugins/ad_disclaimer-text.babel.js
@@ -0,0 +1,31 @@
+videojs.registerPlugin('adDisclaimer', function() {
+  this.on('loadedmetadata', () => {
+    const playerId = this.id();
+    const playerControlBarContainer = document.querySelectorAll('#' + playerId + ' .vjs-spacer')[1];
+    const adDisclaimerContainer = document.querySelector('.ad-disclaimer');
+
+    this.on('ima3-started', () => {
+      const adDisclaimerElement = () => {
+        const disclaimerEl = document.createElement('p');
+        disclaimerEl.setAttribute('class', 'ad-disclaimer');
+        disclaimerEl.innerText = 'Advertisement';
+        return disclaimerEl;
+      };
+      const appendAdDisclaimer = (spacerEl, disclaimerEl) => {
+        spacerEl.appendChild(disclaimerEl);
+      };
+      const showAdDisclaimer = () => {
+        appendAdDisclaimer(playerControlBarContainer, adDisclaimerElement());
+      };
+      if (adDisclaimerContainer) {
+        showAdDisclaimer();
+      }
+    });
+    this.on('ima3-complete', () => {
+      const hideAdDisclaimer = () => {
+        playerControlBarContainer.remove();
+      };
+      hideAdDisclaimer();
+    });
+  });
+});

643eb515c83f18daccf5bbf256fb960b98bc21b6
mikejcraig <mike.craig@icloud.com>
2021-02-18 19:32:56 -0500

    Update mobile-ads-preroll-test.js

--- plugins/mobileAdsPreroll/mobile-ads-preroll-test.js
+++ plugins/mobileAdsPreroll/mobile-ads-preroll-test.js
@@ -11,7 +11,7 @@ videojs.registerPlugin('mobileAdsPreroll', function () {
   var iosCustomPlaybackSetting = function iosCustomPlaybackSetting() {
     // Disable custom playback for ios
     if (_typeof(window.google.ima) === 'object') {
-      window.google.ima.settings.setDisableCustomPlaybackForIOS10Plus(true);
+   //   window.google.ima.settings.setDisableCustomPlaybackForIOS10Plus(true);
     }
   };
 

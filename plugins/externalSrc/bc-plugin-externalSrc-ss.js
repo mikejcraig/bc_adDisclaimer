a4b46a3a4f2139b77c31ce24eb0040e785d03b01
mikejcraig <mike.craig@icloud.com>
2021-10-06 23:18:40 -0400

    Add files via upload

---
+++ plugins/externalSrc/bc-plugin-externalSrc-ss.js
@@ -0,0 +1,48 @@
+videojs.registerPlugin('externalSrc', function() {
+  var player = this;
+  var gamesStageSite = (window.location.host == 'arenax-testing-games.aarp.org') ? true : true;
+  var gamename = document.querySelector('#aarpAdUnitCustom');
+  var triviaGame = (gamename && gamename.content.match('gamename=aarp-trivia')) ? true : false;
+  var pageRefresh = (typeof performance == 'object') ? performance.navigation.type : false;
+  var bcplaycapAdPlayed = sessionStorage.getItem('bcplaycapAdPlayed');
+
+  var insertButton = function() {
+    var vidJs = window.document.getElementsByTagName('video-js')[0];
+    var iDiv = window.document.createElement('div');
+        iDiv.setAttribute('aria-hidden', 'false');
+        iDiv.className = 'vjs-big-play-button altstyle';
+    var iDivP = window.document.createElement('p');
+        iDivP.setAttribute('aria-hidden', 'true');
+        iDivP.className = 'vjs-icon-placeholder';
+    iDiv.append(iDivP);
+    vidJs.append(iDiv);
+  }
+
+    if(gameTarget){
+        if(!!cookieValue && bcplaycapAdPlayed == 'true') {
+      console.log('NavigationType ' + pageRefresh);  
+            window.document.getElementsByClassName('vjs-big-play-button')[0].remove();
+            setTimeout(function(){
+              insertButton();
+              $aarpe( '.vjs-big-play-button.altstyle' ).click(function( event ) {
+                event.preventDefault();
+                myPlayer = videojs.getAllPlayers()[0];
+                myPlayer.src({ type: "video/mp4", src: null });
+              });
+            }, 1000);
+        } else {
+    sessionStorage.removeItem('bcplaycapAdPlayed');
+          setFrequencyCap();
+        }
+    }
+
+  // var playerSource = (player.tagAttributes.src) ? player.tagAttributes.src : false;
+  // if(playerSource){
+  //   console.log('playerSource' + playerSource);
+  //   player.src(playerSource);
+  // }
+  
+  player.on('adend', function(evt) {
+      window.location.href = '/games-play/ss-games/path-finder/'
+  });
+});

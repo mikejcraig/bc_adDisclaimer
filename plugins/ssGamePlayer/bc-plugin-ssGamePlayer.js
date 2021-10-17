1fd88391bc308fe96ba22b99fd17609806d539b7
mikejcraig <mike.craig@icloud.com>
2021-10-17 14:03:25 -0400

    Update bc-plugin-ssGamePlayer.js

--- plugins/ssGamePlayer/bc-plugin-ssGamePlayer.js
+++ plugins/ssGamePlayer/bc-plugin-ssGamePlayer.js
@@ -16,18 +16,18 @@ videojs.registerPlugin('ssGamePlayer', function() {
     vidJs.append(iDiv);
   }
 
-// document.querySelectorAll(".sharp-c-button")[1].addEventListener("click", function(event) {
-//      var videojsId = document.querySelector('.video-js video');
-//          videojsId.play();
-//          event.preventDefault();
-// }, false);
-
-document.querySelector('a.sharp-c-button.sharp-js-demographic-trigger.sharp-c-link').addEventListener("click", function(event) {
+document.querySelectorAll(".sharp-c-button")[0].addEventListener("click", function(event) {
      var videojsId = document.querySelector('.video-js video');
-console.log('click blocked');
-  videojsId.play();
+         videojsId.play();
          event.preventDefault();
 }, false);
+
+// document.querySelector('a.sharp-c-button.sharp-js-demographic-trigger.sharp-c-link').addEventListener("click", function(event) {
+//      var videojsId = document.querySelector('.video-js video');
+// console.log('click blocked');
+//   videojsId.play();
+//          event.preventDefault();
+// }, false);
   
   player.on('adend', function(evt) {
    // if(window.location.href == 'https://stayingsharp-l.aarp.org/articles/mindful/long-term-meditators.html'){

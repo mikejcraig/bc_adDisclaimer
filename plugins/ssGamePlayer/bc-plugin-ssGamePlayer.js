2446ad56afa14276aa23e0c68e07a370ccc45493
mikejcraig <mike.craig@icloud.com>
2021-10-17 14:31:25 -0400

    Update bc-plugin-ssGamePlayer.js

--- plugins/ssGamePlayer/bc-plugin-ssGamePlayer.js
+++ plugins/ssGamePlayer/bc-plugin-ssGamePlayer.js
@@ -16,22 +16,23 @@ videojs.registerPlugin('ssGamePlayer', function() {
     vidJs.append(iDiv);
   }
 
-document.querySelectorAll(".sharp-c-button")[0].addEventListener("click", function(event) {
-     var videojsId = document.querySelector('.video-js video');
-         videojsId.play();
-         event.preventDefault();
-}, false);
-
-// document.querySelector('a.sharp-c-button.sharp-js-demographic-trigger.sharp-c-link').addEventListener("click", function(event) {
+// document.querySelectorAll(".sharp-c-button")[0].addEventListener("click", function(event) {
 //      var videojsId = document.querySelector('.video-js video');
-// console.log('click blocked');
-//   videojsId.play();
+//          videojsId.play();
 //          event.preventDefault();
 // }, false);
+
+document.querySelector('a.sharp-c-button.sharp-js-demographic-trigger.sharp-c-link').addEventListener("click", function(event) {
+     var videojsId = document.querySelector('.video-js video');
+console.log('click blocked');
+  videojsId.play();
+         event.preventDefault();
+}, false);
   
   player.on('adend', function(evt) {
    // if(window.location.href == 'https://stayingsharp-l.aarp.org/articles/mindful/long-term-meditators.html'){
-      window.location.href = '/games-play/ss-games/path-finder/'
+   //   window.location.href = '/games-play/ss-games/path-finder/';
+    window.location.href = 'https://stayingsharp-s.aarp.org/games-play/ss-games/path-finder/';
     console.log('page forwarded');
 
    // }

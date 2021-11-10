7f7e5a323bac6aefeffc10f9605fb34cdd43773a
mikejcraig <mike.craig@icloud.com>
2021-11-10 12:16:42 -0500

    Add files via upload

--- plugins/ssGamePlayer/ad_disclaimer-text-ads-started.js
+++ plugins/ssGamePlayer/ad_disclaimer-text-ads-started.js
@@ -1,1 +1,28 @@
-"use strict";videojs.registerPlugin("adDisclaimer",function(){var t=this;this.on("loadedmetadata",function(){var e=t.id(),i=document.querySelectorAll("#"+e+" .vjs-spacer")[1];t.on("ads-ad-started",function(){var e,t;e=i,(t=document.createElement("p")).setAttribute("class","ad-disclaimer"),t.innerText="Advertisement",t=t,e.appendChild(t)}),t.on("ads-ad-ended",function(){i.remove()})})});
+videojs.registerPlugin('adDisclaimer', function() {
+  // this.on('loadedmetadata', () => {
+    // const playerId = this.id();
+    const playerControlBarContainer = document.querySelectorAll('.vjs-spacer')[1];
+
+    // this.on('ima3-started', () => {
+      var adDisclaimerElement = function() {
+        const disclaimerEl = document.createElement('p');
+        disclaimerEl.setAttribute('class', 'ad-disclaimer');
+        disclaimerEl.innerText = 'Advertisement';
+        return disclaimerEl;
+      };
+      var appendAdDisclaimer = function(spacerEl, disclaimerEl) {
+        spacerEl.appendChild(disclaimerEl);
+      };
+      var showAdDisclaimer = function() {
+        appendAdDisclaimer(playerControlBarContainer, adDisclaimerElement());
+      };
+      showAdDisclaimer();
+    // });
+    // this.on('ima3-complete', () => {
+    //   const hideAdDisclaimer = () => {
+    //     playerControlBarContainer.remove();
+    //   };
+    //   hideAdDisclaimer();
+    // });
+  // });
+});

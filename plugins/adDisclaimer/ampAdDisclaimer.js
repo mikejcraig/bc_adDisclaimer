fbe6bceba06498dfaa0e8c490e422c8def1fedc3
mikejcraig <mike.craig@icloud.com>
2021-12-13 22:14:12 -0500

    Add files via upload

--- plugins/adDisclaimer/ampAdDisclaimer.js
+++ plugins/adDisclaimer/ampAdDisclaimer.js
@@ -2,8 +2,8 @@ videojs.registerPlugin('ampAdDisclaimer', function() {
   var player = this;
   player.on('ads-request', function(evt) {
 		const disclaimerElement = document.createElement('p');
-        disclaimerEl.setAttribute('class', 'ad-disclaimer');
-        disclaimerEl.textContent = 'Advertisement';
+        disclaimerElement.setAttribute('class', 'ad-disclaimer');
+        disclaimerElement.textContent = 'Advertisement';
         const playerControlBarContainer = document.querySelector(`#${player.id()} .vjs-spacer`);
         playerControlBarContainer.appendChild(disclaimerElement);
   })      

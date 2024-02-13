3dd39710a62636392c9918bc70dc3699849ad3d7
mikejcraig <mike.craig@icloud.com>
2024-02-13 05:00:17 -0500

    Update bc-plugin-externalSrc-v2-1-cap5-global-ux-gpr4.js

--- plugins/externalSrc/bc-plugin-externalSrc-v2-1-cap5-global-ux-gpr4.js
+++ plugins/externalSrc/bc-plugin-externalSrc-v2-1-cap5-global-ux-gpr4.js
@@ -144,4 +144,10 @@ player.on('ima3-started', function () {
       //         }));
       // });
 //  }
+	 if (gamesStageSiteEnabled2) {
+
+  document.querySelector('#container-81ea044270').setAttribute('data-scroll', false);
+  document.querySelector('#container-81ea044270').id = '';
+  AARP.header.sticky = false;
+}
 });
